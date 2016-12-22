bc31f7041d9d8fc589c8bd554cba52f1f84aa994
Thomas Dubiel <td@crazynexus.de>
2016-12-22 11:56:24 +0100

    initial commit

---
+++ WebCalls/Main.cs
@@ -0,0 +1,21 @@
+﻿//
+// Main.cs
+//
+// Created by Thomas Dubiel on 22.12.2016
+// Copyright 2016 Thomas Dubiel. All rights reserved.
+//
+using UIKit;
+
+namespace WebCalls
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main(string[] args)
+		{
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main(args, null, "AppDelegate");
+		}
+	}
+}
---
+++ WebCalls/ViewController.cs
@@ -0,0 +1,32 @@
+﻿//
+// ViewController.cs
+//
+// Created by Thomas Dubiel on 22.12.2016
+// Copyright 2016 Thomas Dubiel. All rights reserved.
+//
+using System;
+
+using UIKit;
+
+namespace WebCalls
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
